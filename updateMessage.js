7aba5662abfe0434966de09b77b6a44b9c1621f6
лагзя <lagzya@gmail.com>
2022-12-20 23:28:16 +0300

    Update animation seconds

--- updateMessage.js
+++ updateMessage.js
@@ -1,9 +1,11 @@
 function test(){
+    let seconds = new Date().getSeconds() * 6;
     setInterval(() => {
-        var date = new Date();
+        let date = new Date();
         document.getElementById("hours").style.transform = `rotate(${date.getHours() * 30 + date.getMinutes() / 2}deg)`;
         document.getElementById("minutes").style.transform = `rotate(${(date.getMinutes() * 6 + date.getSeconds() * 0.1)}deg)`;
-        document.getElementById("seconds").style.transform = `rotate(${date.getSeconds() * 6}deg)`;
-    });
+        document.getElementById("seconds").style.transform = `rotate(${seconds}deg)`;
+        seconds += 6;
+    }, 1000);
 }
 test()
