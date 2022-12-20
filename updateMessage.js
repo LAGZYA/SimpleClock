865c1da1e05120bbf8df7b588896e81cb6f7b80c
лагзя <lagzya@gmail.com>
2022-12-20 23:42:11 +0300

    Fixed a delay in the operation of the clock

--- updateMessage.js
+++ updateMessage.js
@@ -1,5 +1,9 @@
 function test(){
-    let seconds = new Date().getSeconds() * 6;
+    let seconds = new Date().getSeconds() * 6 + 6;
+    document.getElementById("hours").style.transform = `rotate(${new Date().getHours() * 30 + new Date().getMinutes() / 2}deg)`;
+    document.getElementById("minutes").style.transform = `rotate(${(new Date().getMinutes() * 6 + new Date().getSeconds() * 0.1)}deg)`;
+    document.getElementById("seconds").style.transform = `rotate(${seconds}deg)`;
+    seconds += 6;
     setInterval(() => {
         let date = new Date();
         document.getElementById("hours").style.transform = `rotate(${date.getHours() * 30 + date.getMinutes() / 2}deg)`;
